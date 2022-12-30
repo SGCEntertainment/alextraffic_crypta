d7ce8d27510dada7964ae425c51433b60f813a29
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-12-30 14:59:53 +0300

    update

--- Assets/alextraffic_crypta/Scripts/Navigation.cs
+++ Assets/alextraffic_crypta/Scripts/Navigation.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Navigation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Button[] NavButtons { get; set; }
+    private Image[] NavImages { get; set; }
+    private Text[] NavTexts { get; set; }
+
+    [SerializeField] Color activeColor;
+
+    private void Awake()
     {
-        
+        NavButtons = new Button[transform.childCount];
+
+        NavImages = new Image[transform.childCount];
+        NavTexts = new Text[transform.childCount];
+
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            NavButtons[i] = transform.GetChild(i).GetComponent<Button>();
+
+            NavImages[i] = transform.GetChild(i).GetChild(0).GetComponent<Image>();
+            NavTexts[i] = transform.GetChild(i).GetChild(1).GetComponent<Text>();
+        }
+
+        SetPage(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetPage(int id)
     {
-        
+        for (int i = 0; i < NavButtons.Length; i++)
+        {
+            NavImages[i].color = i == id ? activeColor : Color.black;
+            NavTexts[i].color = i == id ? activeColor : Color.black;
+        }
     }
 }
